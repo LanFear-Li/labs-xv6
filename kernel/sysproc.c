87b9814beab3bd1b7b6d97e41011895c5c26e5b1
LanFear-Li <1371705749@qq.com>
2021-10-26 14:34:53 +0800

    syscall files added

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -79,3 +79,14 @@ uint64 sys_uptime(void) {
     release(&tickslock);
     return xticks;
 }
+
+uint64 sys_trace(void) {
+    int mask;
+    if (argint(0, &mask) < 0) {
+        return -1;
+    }
+
+    struct proc *p = myproc();
+    p->mask = mask;
+    return mask;
+}
